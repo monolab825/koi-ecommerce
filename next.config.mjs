8318914d6a7a578fcd8ef4ab02c5e71525817a5c
monolab825 <p.smiles777@gmail.com>
2024-06-20 08:45:45 +0700

    fix singout in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === "true",
 });
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 export default bundleAnalyzer(nextConfig);
